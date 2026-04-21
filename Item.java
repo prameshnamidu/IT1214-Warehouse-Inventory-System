6b0ddccf8460c2f84fafa7bead7667859ac639a8
prameshnamidu <prameshnamidu@gmail.com>
2026-04-21 23:11:17 +0530

    Item.java

--- Item.java
+++ Item.java
@@ -1,10 +1,10 @@
 public class Item {
-
     private String itemId;
     private String itemName;
     private int quantity;
     private double price;
 
+    // Constructor
     public Item(String itemId, String itemName, int quantity, double price) {
         this.itemId = itemId;
         this.itemName = itemName;
@@ -12,42 +12,33 @@ public class Item {
         this.price = price;
     }
 
+    // Getters and Setters
     public String getItemId() {
         return itemId;
-    }
-
-    public void setItemId(String itemId) {
-        this.itemId = itemId;
     }
 
     public String getItemName() {
         return itemName;
     }
 
-    public void setItemName(String itemName) {
-        this.itemName = itemName;
-    }
-
     public int getQuantity() {
         return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
     }
 
     public double getPrice() {
         return price;
     }
 
-    public void setPrice(double price) {
-        this.price = price;
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
     }
 
+    // toString method
+    @Override
     public String toString() {
         return "ID: " + itemId +
-               " Name: " + itemName +
-               " Quantity: " + quantity +
-               " Price: " + price;
+               ", Name: " + itemName +
+               ", Quantity: " + quantity +
+               ", Price: " + price;
     }
 }
