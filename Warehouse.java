400ee9e99588e22b194515ad1d11184aedefa661
prameshnamidu <prameshnamidu@gmail.com>
2026-04-21 23:13:26 +0530

    Warehouse.java

--- Warehouse.java
+++ Warehouse.java
@@ -1,86 +1,71 @@
 import java.util.Scanner;
 
 public class Warehouse {
-
     public static void main(String[] args) {
-
         Inventory inventory = new Inventory();
         Scanner sc = new Scanner(System.in);
 
         int choice;
 
         do {
-
-            System.out.println("\nWarehouse Inventory System");
-            System.out.println("1 Add Item");
-            System.out.println("2 Remove Item");
-            System.out.println("3 Update Quantity");
-            System.out.println("4 Search by ID");
-            System.out.println("5 Search by Name");
-            System.out.println("6 Display Items");
-            System.out.println("7 Exit");
-
+            System.out.println("\n--- Warehouse Menu ---");
+            System.out.println("1. Add Item");
+            System.out.println("2. Remove Item");
+            System.out.println("3. Update Quantity");
+            System.out.println("4. Search Item");
+            System.out.println("5. Display All Items");
+            System.out.println("6. Exit");
             System.out.print("Enter choice: ");
+
             choice = sc.nextInt();
+            sc.nextLine(); // clear buffer
 
-            switch(choice) {
-
+            switch (choice) {
                 case 1:
                     System.out.print("Enter ID: ");
-                    String id = sc.next();
-
+                    String id = sc.nextLine();
                     System.out.print("Enter Name: ");
-                    String name = sc.next();
-
+                    String name = sc.nextLine();
                     System.out.print("Enter Quantity: ");
                     int qty = sc.nextInt();
-
                     System.out.print("Enter Price: ");
                     double price = sc.nextDouble();
 
-                    Item item = new Item(id,name,qty,price);
-                    inventory.addItem(item);
+                    inventory.addItem(new Item(id, name, qty, price));
                     break;
 
                 case 2:
-                    System.out.print("Enter ID: ");
-                    inventory.removeItem(sc.next());
+                    System.out.print("Enter ID to remove: ");
+                    inventory.removeItem(sc.nextLine());
                     break;
 
                 case 3:
                     System.out.print("Enter ID: ");
-                    String uid = sc.next();
-
+                    String uid = sc.nextLine();
                     System.out.print("Enter new quantity: ");
                     int newQty = sc.nextInt();
-
-                    inventory.updateQuantity(uid,newQty);
+                    inventory.updateQuantity(uid, newQty);
                     break;
 
                 case 4:
-                    System.out.print("Enter ID: ");
-                    inventory.searchById(sc.next());
+                    System.out.print("Enter ID or Name: ");
+                    inventory.searchItem(sc.nextLine());
                     break;
 
                 case 5:
-                    System.out.print("Enter Name: ");
-                    inventory.searchByName(sc.next());
-                    break;
-
-                case 6:
                     inventory.displayItems();
                     break;
 
-                case 7:
+                case 6:
                     System.out.println("Exiting...");
                     break;
 
                 default:
-                    System.out.println("Invalid choice");
-
+                    System.out.println("Invalid choice!");
             }
 
-        } while(choice != 7);
+        } while (choice != 6);
 
+        sc.close();
     }
 }
